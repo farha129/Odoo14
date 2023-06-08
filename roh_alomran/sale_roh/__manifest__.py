958a1e2c50518f53b64b0e3c2ed8e3bf2c0a9dcf
farha129 <farha129@gmail.com>
2023-06-08 07:40:31 +0300

    ooooobbbppbkk

--- roh_alomran/sale_roh/__manifest__.py
+++ roh_alomran/sale_roh/__manifest__.py
@@ -14,7 +14,7 @@
 
         'security/ir.model.access.csv',
         'data/data_sale.xml',
-        'data/produt_demo.xml',
+        'data/product_demo.xml',
         'views/sale_views.xml',
         'views/product_views.xml',
         'report/contract.xml',
